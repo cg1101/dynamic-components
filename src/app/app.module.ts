5f06c84ceb4012786242fd84e5f4fbd6b6ea93df
gang.cheng <gang.cheng@rokt.com>
2018-03-07 08:18:09 +1100

    format update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
